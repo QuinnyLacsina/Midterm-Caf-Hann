904ffc2adb1383747b01512304afe0ffd8ee7365
QuinnyLacsina <98503897+QuinnyLacsina@users.noreply.github.com>
2023-03-25 01:54:57 +0800

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  images: string;
 }
 
 export const products: Product[] = [
@@ -12,6 +13,7 @@ export const products: Product[] = [
     price: 170,
     description:
       'is an espresso-based coffee drink that is prepared with steamed milk foam. Variations of the drink involve the use of cream instead of milk, using non-dairy milk substitutes and flavoring with cinnamon or chocolate powder.',
+    images: 'assets/coffee1.webp',
   },
   {
     id: 2,
@@ -19,6 +21,7 @@ export const products: Product[] = [
     price: 150,
     description:
       'is a coffee drink prepared with hot water, giving it a similar strength to, but different flavor from, traditionally brewed coffee. The strength of it varies with the number of shots of espresso and the amount of water added.',
+    images: 'assets/coffee2.webp',
   },
   {
     id: 3,
@@ -26,6 +29,7 @@ export const products: Product[] = [
     price: 200,
     description:
       'is a chocolate-flavoured warm beverage that is a variant of a caffè latte, commonly served in a glass rather than a mug.',
+    images: 'assets/coffee3.webp',
   },
   {
     id: 4,
@@ -33,5 +37,6 @@ export const products: Product[] = [
     price: 250,
     description:
       'is a coffee with a topping of espresso on roughly equal amounts of condensed milk.  don’t mix, and barista usually serves this drink in a tumbler glass to manifest the beautiful layering of coffee and milk.',
+    images: 'assets/coffee4.webp',
   },
 ];
